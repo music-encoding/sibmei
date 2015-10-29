abe2c82d2e4b17c3546d8da584237301e3c61e78
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-10-29 00:21:37 -0700

    New: Batch processing plugins

--- plgconfig.js
+++ plgconfig.js
@@ -2,7 +2,7 @@ var config = {
   plgPath:         process.env.HOME + '/Library/Application Support/Avid/Sibelius 7.5/Plugins',
   plgCategory:    'MEI Export',
   pluginFilename: 'sibmei2.plg',
-  linkLibraries: ['libmei.plg'],
+  linkLibraries: ['libmei.plg', 'sibmei_batch_mxml.plg', 'sibmei_batch_sib.plg'],
   importDir:      './import',
   buildDir:       './build',
   srcDir:         './src',
