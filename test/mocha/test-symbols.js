67a7ae89422afe2f7304e0f8c83c7db2090a3ebd
annplaksin <Plaksin@MaxWeberStiftung.de>
2020-06-15 13:16:57 +0200

    Added tests for every symbol

--- test/mocha/test-symbols.js
+++ test/mocha/test-symbols.js
@@ -6,20 +6,36 @@ const utils = require('./utils');
 
 const meiSymbols = utils.getTestMeiDom('symbols.mei');
 
-describe("Expected attributes for symbols (mordents and turns)", () => {
-    const mordents = xpath.evaluateXPath('//*:mordent', meiSymbols);
-    const turns = xpath.evaluateXPath('//*:turn', meiSymbols);
-    it("Mordent has @form='upper'", () => {
-      utils.assertAttrOnElements(mordents, [0], 'form', 'upper');
+describe("Symbols", function() {
+    describe("Control events: Expected attributes (mordents and turns)", function() {
+        const mordents = xpath.evaluateXPath('//*:mordent', meiSymbols);
+        const turns = xpath.evaluateXPath('//*:turn', meiSymbols);
+        it("Mordent has @form='upper'", function() {
+          utils.assertAttrOnElements(mordents, [0], 'form', 'upper');
+        });
+        it("Inverted mordent has @form='lower'", function() {
+          utils.assertAttrOnElements(mordents, [1], 'form', 'lower');
+        });
+        it("Turn has @form='upper'", function() {
+          utils.assertAttrOnElements(turns, [0], 'form', 'upper');
+        });
+        it("Inverted turn has @form='lower'", function() {
+          utils.assertAttrOnElements(turns, [1], 'form', 'lower');
+        });
+      });
+    describe("Modifiers (children of note): Articulations", function() {
+        var artics = xpath.evaluateXPath('//*:artic', meiSymbols);
+        it("<artic> is child of <note>", function() {
+            for (let count = 0; count < artics.length; count++) {
+                assert.strictEqual(artics[count].parentNode.localName, "note", '<artic> ${i} is not a child of <note>');
+            }
+        });
+        it("every <artic> has @artic", function() {
+            utils.assertHasAttr(artics, "artic");
+        });
+        it("<artic> with @place", function() {
+            utils.assertElsHasAttr(artics, [9, 10, 11, 12, 13, 14], 'place');
+        });
     });
-    it("Inverted mordent has @form='lower'", () => {
-      utils.assertAttrOnElements(mordents, [1], 'form', 'lower');
-    });
-    it("Turn has @form='upper'", () => {
-      utils.assertAttrOnElements(turns, [0], 'form', 'upper');
-    });
-    it("Inverted turn has @form='lower'", () => {
-      utils.assertAttrOnElements(turns, [1], 'form', 'lower');
-    });
-  });
+});
   
